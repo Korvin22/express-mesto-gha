0f0b969a31c174a683a9b64442b9491f02025dae
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2022-12-09 05:30:31 +0300

    eslint fixes

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const bodyParser = require("body-parser");
-const router = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes');
 
 const PORT = 3000;
 const app = express();
 
-/*app.use(express.static(path.join(__dirnamey, 'public')));*/
+/* app.use(express.static(path.join(__dirnamey, 'public'))); */
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '638998e8e5e932e174b0972e' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '638998e8e5e932e174b0972e', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
 });
-app.use("/", router);
+app.use('/', router);
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser:true,
-}, ()=> {
+  useNewUrlParser: true,
+}, () => {
   console.log('base are connected');
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
   });
-})
+});
