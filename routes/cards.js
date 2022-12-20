56e80cb26a0a865bd0197709f758cf3bd268a7e9
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2022-12-20 05:35:24 +0300

    celebrate library added

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getAllCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getAllCards);
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
 router.post('/', createCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 
 router.use((req, res) => res.status(404).send({ message: 'Роут не найден' }));
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,28 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getAllUsers, getUser, createUser, updateUser, updateAvatar, getCurrentUser,
 } = require('../controllers/users');
 
 router.get('/', getAllUsers);
-router.get('/:id', getUser);
+router.get('/:id', celebrate({
+  body: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), getUser);
 router.post('/', createUser);
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
 router.get('/me', getCurrentUser);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().min(2),
+  }),
+}), updateAvatar);
 
 router.use((req, res) => res.status(404).send({ message: 'Роут не найден' }));
 module.exports = router;
